79f85e17498bbb4efb2257d4ee96915207bf5ea8
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2010-04-01 20:23:24 -0500

    adding syncr app

---
+++ lib/syncer.py
@@ -0,0 +1,19 @@
+from syncr.app.tweet import TwitterSyncr
+from syncr.app.flickr import FlickrSyncr
+from django.conf import settings
+
+
+def sync_twitter():
+    username = settings.TWITTER['username']
+    password = settings.TWITTER['password']
+    ts = TwitterSyncr(username, password)
+    ts.syncUser(username)
+    ts.syncTwitterUserTweets(username)
+
+
+def sync_flickr():
+    key = settings.FLICKR['key']
+    secret = settings.FLICKR['secret']
+    username = settings.FLICKR['username']
+    fs = FlickrSyncr(key, secret)
+    fs.syncRecentPhotos(username)
