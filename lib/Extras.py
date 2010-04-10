cdfcfdbc2ffa00c51a48d73bb178d261b2ce4059
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2010-04-10 10:46:31 -0500

    removing syncr from lawrencetrailhawk repo, should be a dependency not included

--- lib/Extras.py
+++ lib/Extras.py
@@ -1,18 +1,18 @@
 from lawrencetrailhawks.events.models import Event
 from lawrencetrailhawks.runs.models import Runs
+from lawrencetrailhawks.syncr.twitter.models import Tweet
 import datetime
 
 def get_latest():
-	latest_event = Event.objects.filter(date__gte=datetime.datetime.now()).order_by('date')
-	if latest_event: 
-	    latest_event = latest_event[0]
-	else:
-	    latest_event = []
-	
-	todays_run = Runs.objects.filter(run_date=datetime.datetime.now().strftime("%A"))
-	other_news = ""
-	print todays_run
-	return {"latest_event": latest_event,
-	        "todays_run": todays_run,
-	        "other_news": other_news,
-	       }
+    latest_event = Event.objects.filter(date__gte=datetime.datetime.now()).order_by('date').latest('date')
+    todays_run = Runs.objects.filter(run_date=datetime.datetime.now().strftime("%A")).latest('run_date')
+    other_news = ""
+    tweets = Tweet.objects.all()
+    return {"latest_event": latest_event,
+            "todays_run": todays_run,
+            "other_news": other_news,
+            "tweets" : tweets,
+           }
+           
+
+    
