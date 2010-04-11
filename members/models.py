a7ca21505d9680b4d3d94d4fbbe10435b8e1a7b7
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2010-04-11 14:21:40 -0500

    making minor changes to model names

--- members/models.py
+++ members/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Member(models.Model):
-    username = models.ForeignKey(User, unique=True)
+    username = models.ForeignKey(User)
     hawk_name = models.CharField(max_length=50)
     phone = models.PositiveIntegerField()
     address = models.TextField()
@@ -14,11 +15,16 @@ class Member(models.Model):
     #     verbose_name_plural = "Members"
     
     def __unicode__(self):
-        return "%s %s" % (self.first_name, self.last_name)
-
+           return "%s %s" % (self.username.first_name, self.username.last_name)
+   
         
     @models.permalink   
     def get_absolute_url(self):
         """docstring for get_absolute_url"""
         return ('member_detail', (), { 'object_id': self.pk } )
+    
+    @property
+    def date_expires(self):
+        date_expires = datetime.date(self.date_paid.year+1, self.date_paid.month, self.date_paid.day)
+        return date_expires
         
