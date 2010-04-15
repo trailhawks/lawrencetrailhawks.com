753ede0f4938d878a6a99c6bc5bfe77f67e0daf8
nick <nlang@ljworld.com>
2010-04-15 16:23:32 -0500

    fixed the way the replace char filter works so it works properly. Also changed some if tags to ifequals tags in the template.

--- races/templatetags/races_extras.py
+++ races/templatetags/races_extras.py
@@ -3,9 +3,11 @@ from django import template
 register = template.Library()
 
 @register.filter(name="replace_char")
-def replace_char(value, arg1, arg2):
+def replace_char(value, arg):
     """
-    Replaces a character with another
+    Replaces a character with another char
+    value = value to be filtered
+    arg = string of len 2, first char to be replaced with second char 
     """
-    return value.replace(arg1, arg2)
+    return value.replace(arg[0], arg[1])
     
