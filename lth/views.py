a83df2cf89457e7e58b4e0422441f2c39db09ad6
Nick Lang <nick.lang@gmail.com>
2010-11-09 17:03:57 +0000

    more data for the members report

--- lth/views.py
+++ lth/views.py
@@ -5,6 +5,21 @@ from lawrencetrailhawks.members.models import Member
 import csv
 import StringIO
 
+PRESIDENT = 1
+VICE_PRESIDENT = 2
+TREASURER = 3
+SECRETARY = 4
+WEB_MASTER = 5
+POSITION_CHOICES = (
+    (PRESIDENT, "President"),
+    (VICE_PRESIDENT, "Vice President"),
+    (TREASURER, "Treasurer"),
+    (SECRETARY, "Secretary"),
+    (WEB_MASTER, "Web Master"),
+)
+
+
+
 @login_required
 def member_list(request):
     response = HttpResponse(mimetype='text/csv')
@@ -12,13 +27,17 @@ def member_list(request):
 
     members = Member.objects.filter(active=True)
     member_list = csv.writer(response)
+    member_list.writerow(["First Name", "Last Name", "Club Officer Title", "Address", "Email Address","Date paid", "Member Since",  "Dues Due"])
 
     for member in members:
         member_list.writerow([member.first_name,
-                             member.hawk_name,
                              member.last_name,
+                             dict(POSITION_CHOICES).get(member.position, None),
+                             member.address,
+                             member.email,
                              member.date_paid,
-                             member.member_since])
+                             member.member_since,
+                             member.date_expires])
     
     return response
     
