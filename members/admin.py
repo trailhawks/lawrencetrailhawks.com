0d78848e59ca622ce2038c82ccf693f93592b326
jefftriplett <jeff.triplett@gmail.com>
2011-11-18 08:55:30 -0600

    Adds search and active filter to member admin.

--- members/admin.py
+++ members/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+
 from lawrencetrailhawks.members.models import Member
 
+
+class MemberInline(admin.TabularInline):
+    model = Member
+
+
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__','first_name', 'last_name', 'phone', 'date_paid', 'active')
-    list_filter = ('date_paid',)
+    list_display = ('__unicode__', 'first_name', 'last_name', 'phone', 'date_paid', 'active')
+    list_filter = ('date_paid', 'active')
+    search_fields = ('first_name', 'last_name')
+
 
 admin.site.register(Member, MemberAdmin)
