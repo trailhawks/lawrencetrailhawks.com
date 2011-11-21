2ef09a8202fe3b395ed43f58dd4ca0e26019ed95
jefftriplett <jeff.triplett@gmail.com>
2011-11-21 11:40:08 -0600

    Adds hawk name and references position and gender choices from Member models.

--- lth/views.py
+++ lth/views.py
@@ -1,27 +1,10 @@
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-import datetime
-from lawrencetrailhawks.members.models import Member
 import csv
-import StringIO
 
-PRESIDENT = 1
-VICE_PRESIDENT = 2
-TREASURER = 3
-SECRETARY = 4
-WEB_MASTER = 5
-POSITION_CHOICES = (
-    (PRESIDENT, "President"),
-    (VICE_PRESIDENT, "Vice President"),
-    (TREASURER, "Treasurer"),
-    (SECRETARY, "Secretary"),
-    (WEB_MASTER, "Web Master"),
-)
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-GENDER_CHOICES = (
-    (1, "Male"),
-    (2, "Female"),
-)
+from lawrencetrailhawks.members.models import Member
+
 
 @login_required
 def member_list(request):
@@ -30,13 +13,27 @@ def member_list(request):
 
     members = Member.objects.active()
     member_list = csv.writer(response)
-    member_list.writerow(["First Name", "Last Name", "Gender", "Club Officer Title", "Address", "Address2", "City", "State", "Zip", "Email Address","Date paid", "Member Since",  "Dues Due"])
+    member_list.writerow(["First Name",
+                         "Last Name",
+                         "Hawk Name",
+                         "Gender",
+                         "Club Officer Title",
+                         "Address",
+                         "Address2",
+                         "City",
+                         "State",
+                         "Zip",
+                         "Email Address",
+                         "Date paid",
+                         "Member Since",
+                         "Dues Due"])
 
     for member in members:
         member_list.writerow([member.first_name,
                              member.last_name,
-                             dict(GENDER_CHOICES).get(member.gender, None),
-                             dict(POSITION_CHOICES).get(member.position, None),
+                             member.hawk_name,
+                             dict(Member.GENDER_CHOICES).get(member.gender, None),
+                             dict(Member.POSITION_CHOICES).get(member.position, None),
                              member.address,
                              member.address2,
                              member.city,
@@ -48,6 +45,3 @@ def member_list(request):
                              member.date_expires])
 
     return response
-
-
-
