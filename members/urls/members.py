28ffc1a6b4d980100f950e8ed7b74c8262a49315
jefftriplett <jeff.triplett@gmail.com>
2011-11-21 11:57:54 -0600

    Fixed member ordering.

--- members/urls/members.py
+++ members/urls/members.py
@@ -1,13 +1,13 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, patterns
 
 from lawrencetrailhawks.members.models import Member
 
 
 members_info_dict = {
-    'queryset': Member.objects.all(),
+    'queryset': Member.objects.all().order_by('last_name', 'first_name'),
 }
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', members_info_dict, 'member_list'),
-    (r'^(?P<object_id>[-\w]+)/$', 'lawrencetrailhawks.members.views.member_detail', members_info_dict, 'member_detail'),
+    url(r'^$', 'django.views.generic.list_detail.object_list', members_info_dict, 'member_list'),
+    url(r'^(?P<object_id>[-\w]+)/$', 'lawrencetrailhawks.members.views.member_detail', members_info_dict, 'member_detail'),
 )
