3df728ee2919db5c089f63b5733a91e0ef84c7f9
jefftriplett <jeff.triplett@gmail.com>
2012-01-20 19:08:10 -0600

    Photo page grabs 8 photos instead of 12 and displayed them 2 wide instead of 4.

--- photos/views.py
+++ photos/views.py
@@ -4,7 +4,7 @@ from django.template import Context, loader
 from syncr.flickr.models import Photo
 
 def photo_test1(request):
-    photos = Photo.objects.all().order_by('?')[:12]
+    photos = Photo.objects.all().order_by('?')[:8]
     t = loader.get_template('photos.html')
     c = Context({
         "photos": photos,
