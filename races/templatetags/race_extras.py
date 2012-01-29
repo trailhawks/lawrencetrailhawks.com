ffd34a51b7232cbe4790739c6a9d205c37135281
jefftriplett <jeff.triplett@gmail.com>
2012-01-29 15:19:14 -0600

    Update get_latest_race template tag to handle latest_races and latest_posts a bit different and not error on the home page loading.

--- races/templatetags/race_extras.py
+++ races/templatetags/race_extras.py
@@ -1,26 +1,39 @@
+import datetime
+
+from django.template import Library, Node
+
 from races.models import Race
 from blog.models import Post
-from django.template import Library, Node
-import datetime
+
+
 register = Library()
 
-@register.filter(name="replace_char")	 	
+@register.filter(name="replace_char")
 def replace_char(value, arg):
     """
     Replaces a character with another char
     value = value to be filtered
-    arg = string of len 2, first char to be replaced with second char 
+    arg = string of len 2, first char to be replaced with second char
     """
     return value.replace(arg[0], arg[1])
-    
-    
+
+
 class LatestRaceNode(Node):
     def render(self, context):
         try:
-            context['latest_race'] = Race.objects.filter(start_datetime__gte=datetime.datetime.now()).order_by('start_datetime').latest('start_datetime')
-        except:
-            context['latest_post'] = Post.objects.all().order_by('-publish')[0]
+            latest_race = Race.objects.filter(start_datetime__gte=datetime.datetime.now()).order_by('start_datetime').latest('start_datetime')
+            context['latest_race'] = latest_race
+        except Race.DoesNotExist:
+            context['latest_race'] = None
+
+        try:
+            latest_post = context['latest_post'] = Post.objects.all().order_by('-publish')
+            context['latest_post'] = latest_post
+        except Post.DoesNotExist:
+            context['latest_post'] = None
+
         return ''
+
 
 @register.tag
 def get_latest_race(parser, token):
