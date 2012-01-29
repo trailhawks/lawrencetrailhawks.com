ee958cfa03367f86bdb330fd7660d8c53ce7f135
jefftriplett <jeff.triplett@gmail.com>
2012-01-29 15:17:54 -0600

    General PEP8 / pyflakes update.

--- runs/admin.py
+++ runs/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
+
 from lawrencetrailhawks.runs.models import Run, News
 
+
 class NewsAdmin(admin.ModelAdmin):
-    prepopulated_fields = { 'slug': ['title'] }
+    prepopulated_fields = {'slug': ['title']}
+
 
 class NewsInline(admin.StackedInline):
     model = News
 
 class RunAdmin(admin.ModelAdmin):
-    prepopulated_fields = { 'slug': ['name'] }
+    prepopulated_fields = {'slug': ['name']}
     list_display = ('run_date', 'name', 'run_time',)
     list_filter = ('run_date', 'run_time',)
-    inlines = [NewsInline,]
+    inlines = [NewsInline]
+
 
 admin.site.register(Run, RunAdmin)
 admin.site.register(News, NewsAdmin)
