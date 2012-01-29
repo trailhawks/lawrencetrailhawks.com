92f8b1070519bc204d9256a279ec1ea10e115a8a
jefftriplett <jeff.triplett@gmail.com>
2012-01-28 19:26:11 -0600

    PEP8 / pylint update.

--- lib/Extras.py
+++ lib/Extras.py
@@ -1,14 +1,9 @@
-from lawrencetrailhawks.races.models import Race, Result, Racer
-from lawrencetrailhawks.runs.models import Run
 from syncr.twitter.models import Tweet
-from django.contrib.auth.models import User
-from members.models import Member
-import datetime, csv
+
 
 def get_latest():
     tweets = Tweet.objects.all().order_by('-pub_time')
-    
+
     return {
-            "tweets" : tweets,
-           }
-           
+        'tweets': tweets,
+    }
