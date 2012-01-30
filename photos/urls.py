08211001c21d6f2594932c0b52c3a6570fc49f44
jefftriplett <jeff.triplett@gmail.com>
2012-01-30 00:23:23 -0500

    Fix view name and adds photo_list template.

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls.defaults import *
-import datetime
+from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import url
+
 from syncr.flickr.models import Photo
 from syncr.flickr.views import flickr_photo_detail_in_set as photo_detail
+
 
 photo_info_dict = {
     'queryset': Photo.objects.all(),
@@ -9,19 +11,9 @@ photo_info_dict = {
 }
 
 urlpatterns = patterns('',
-    (r'^$', ('lawrencetrailhawks.photos.views.photo_test1')),
-    (r'^(?P<year>\d{4})/$',
-     'django.views.generic.date_based.archive_year',
-     photo_info_dict,
-     'race_archive_year'),
-    (r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
-     'django.views.generic.date_based.archive_month',
-     photo_info_dict,
-     'race_archive_month'),
-    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$',
-     'django.views.generic.date_based.archive_day',
-     photo_info_dict,
-     'race_archive_day'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)/$', photo_detail, 
-     name='photo_detail'),
+    url(r'^$', 'lawrencetrailhawks.photos.views.photo_list'),
+    url(r'^(?P<year>\d{4})/$', 'django.views.generic.date_based.archive_year', photo_info_dict, 'race_archive_year'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'django.views.generic.date_based.archive_month', photo_info_dict, 'race_archive_month'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'django.views.generic.date_based.archive_day', photo_info_dict, 'race_archive_day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)/$', photo_detail, name='photo_detail'),
 )
