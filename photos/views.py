8d9eebe133ec52aebc769263c7d13f23536fc1be
jefftriplett <jeff.triplett@gmail.com>
2012-01-30 00:22:17 -0500

    Updated photos to match bootstrap and adjust the view / templates to be more consistent.

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context
+from django.template import loader
 
 from syncr.flickr.models import Photo
 
-def photo_test1(request):
-    photos = Photo.objects.all().order_by('?')[:8]
-    t = loader.get_template('photos.html')
-    c = Context({
-        "photos": photos,
-    })
 
+def photo_list(request, template_name='photos/photo_list.html'):
+    photos = Photo.objects.all().order_by('?')[:8]
+    t = loader.get_template(template_name)
+    c = Context({
+        'photos': photos,
+    })
     return HttpResponse(t.render(c))
