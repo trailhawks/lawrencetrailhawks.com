bfc069cf430d2a8ea0f6209d26bced23fcd3bc7e
jefftriplett <jeff.triplett@gmail.com>
2012-02-02 00:59:44 -0500

    Temp fix on TwitterNode class to keep it from blowing up on an error.

--- lth/templatetags/lth.py
+++ lth/templatetags/lth.py
@@ -1,17 +1,18 @@
 import feedparser
-from django.template import Library, Node
-import datetime
+
+from dateutil.parser import parse
 from dateutil.tz import *
-from dateutil.parser import *
+from django.template import Library, Node
+
 
 register = Library()
-     
+
 class RRCANewsNode(Node):
     def render(self, context):
         d = feedparser.parse("http://feeds.feedburner.com/RRCA-News?format=xml")
         context['rrca_news'] = d['entries'][:4]
         return ''
-    
+
 def get_rrca_news(parser, token):
     return RRCANewsNode()
 
@@ -20,18 +21,23 @@ class TwitterNode(Node):
         d = feedparser.parse("http://twitter.com/statuses/user_timeline/40239504.rss")
         raw_tweets = d['entries'][:4]
         tweets = []
-        for t in raw_tweets:
-            text = t['title'].replace("trailhawks: ", "")
-            date = parse(t['updated'])
-            tweet = {}
-            tweet['text'] = text
-            tweet['url'] = t['link']
-            tweet['pub_time'] = date.astimezone(gettz())
-            tweets.append(tweet)
-         
-        context['stweets'] = tweets
+        try:
+            for t in raw_tweets:
+                text = t['title'].replace("trailhawks: ", "")
+                date = parse(t['updated'])
+                tweet = {}
+                tweet['text'] = text
+                tweet['url'] = t['link']
+                tweet['pub_time'] = date  # .astimezone(gettz())
+                tweets.append(tweet)
+
+            context['stweets'] = tweets
+
+        except:
+            context['stweets'] = []
+
         return ''
-        
+
 def get_latest_tweets(parser, token):
     return TwitterNode()
 
