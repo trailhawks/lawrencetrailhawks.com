b2cc5c5dcf4ade65c89e41bfbc592fe3425cd9f8
jefftriplett <jeff.triplett@gmail.com>
2012-02-03 12:53:53 -0500

    Adds order field to run models.

--- runs/migrations/0003_auto__add_field_run_order.py
+++ runs/migrations/0003_auto__add_field_run_order.py
@@ -1,22 +1,17 @@
 # encoding: utf-8
-import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
+
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding field 'Run.order'
         db.add_column('runs_run', 'order', self.gf('django.db.models.fields.IntegerField')(default=10), keep_default=False)
 
-
     def backwards(self, orm):
-        
         # Deleting field 'Run.order'
         db.delete_column('runs_run', 'order')
-
 
     models = {
         'auth.group': {
