a39a667107f9e2692968dfe10172c575b1e99c7d
jefftriplett <jeff.triplett@gmail.com>
2012-10-11 09:20:52 -0500

    Fixed an Index bug that kept the site from working with no data. This whole tag need re-thought.

--- races/templatetags/race_extras.py
+++ races/templatetags/race_extras.py
@@ -1,26 +1,36 @@
-from races.models import Race
-from blog.models import Post
-from django.template import Library, Node
 import datetime
+
+from django.template import Library, Node
+
+from blog.models import Post
+from races.models import Race
+
+
 register = Library()
 
-@register.filter(name="replace_char")	 	
+
+@register.filter(name="replace_char")
 def replace_char(value, arg):
     """
     Replaces a character with another char
     value = value to be filtered
-    arg = string of len 2, first char to be replaced with second char 
+    arg = string of len 2, first char to be replaced with second char
     """
     return value.replace(arg[0], arg[1])
-    
-    
+
+
 class LatestRaceNode(Node):
     def render(self, context):
         try:
             context['latest_race'] = Race.objects.filter(start_datetime__gte=datetime.datetime.now()).order_by('start_datetime').latest('start_datetime')
         except:
-            context['latest_post'] = Post.objects.all().order_by('-publish')[0]
+            latest_post = Post.published_objects.all()
+            if len(latest_post):
+                context['latest_post'] = latest_post[0]
+            else:
+                context['latest_post'] = []
         return ''
+
 
 @register.tag
 def get_latest_race(parser, token):
