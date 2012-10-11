9c674c3273fd4c5d2829f01c16ac6eb5d79e265c
jefftriplett <jeff.triplett@gmail.com>
2012-10-11 09:18:31 -0500

    Moved blog.urls.blog to blog.urls.

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template, redirect_to
+from django.views.generic.simple import direct_to_template
+
 from lawrencetrailhawks.lib.Extras import get_latest
-from syncr.flickr.models import Photo, PhotoSet
+
 
 admin.autodiscover()
 
@@ -22,14 +23,14 @@ urlpatterns = patterns('',
     (r'^live/$', direct_to_template, {'template': 'live_coverage.html'}),
     (r'^photos/', include('lawrencetrailhawks.photos.urls.photos')),
     (r'^about/$', direct_to_template, {'template': 'about.html'}),
-    (r'^faq/', include('lawrencetrailhawks.faq.urls.faq') ),
-    (r'^blog/', include('lawrencetrailhawks.blog.urls.blog')),
+    (r'^faq/', include('lawrencetrailhawks.faq.urls.faq')),
+    (r'^blog/', include('lawrencetrailhawks.blog.urls')),
     (r'^comments/', include('django.contrib.comments.urls')),
-    (r'^links/', include('lawrencetrailhawks.links.urls.links') ),
-    (r'^runs/', include('lawrencetrailhawks.runs.urls.runs') ),
-    (r'^members/', include('lawrencetrailhawks.members.urls.members') ),
-    (r'^sponsors/', 'lawrencetrailhawks.sponsors.views.get_sponsors' ),
-    (r'^races/', include('lawrencetrailhawks.races.urls.races') ),
+    (r'^links/', include('lawrencetrailhawks.links.urls.links')),
+    (r'^runs/', include('lawrencetrailhawks.runs.urls.runs')),
+    (r'^members/', include('lawrencetrailhawks.members.urls.members')),
+    (r'^sponsors/', 'lawrencetrailhawks.sponsors.views.get_sponsors'),
+    (r'^races/', include('lawrencetrailhawks.races.urls.races')),
     #(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
     #        {'document_root': settings.STATIC_DOC_ROOT}),
     (r'^contact/$', 'lawrencetrailhawks.members.views.officer_list'),
