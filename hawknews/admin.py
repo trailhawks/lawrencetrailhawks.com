cf022ab0fcd387158b3adb0aa9dc10af288ea804
jefftriplett <jeff.triplett@gmail.com>
2012-10-13 07:55:32 -0500

    Added initial migrations for Hawknews and refactored `draft` field into `status` field.

--- hawknews/admin.py
+++ hawknews/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from lawrencetrailhawks.hawknews.models import HawkNews
+
+from hawknews.models import HawkNews
+
 
 class HawkNewsAdmin(admin.ModelAdmin):
-    prepopulated_fields = { 'slug': ['title'] }
-    list_display = ('title', 'pub_date', 'draft')
-    list_filter = ('pub_date', 'draft')
+    prepopulated_fields = {'slug': ['title']}
+    list_display = ('title', 'pub_date', 'status')
+    list_filter = ('pub_date', 'status')
+
 
 admin.site.register(HawkNews, HawkNewsAdmin)
--- hawknews/models.py
+++ hawknews/models.py
@@ -1,27 +1,50 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class DraftManager(models.Manager):
+
+    def get_query_set(self):
+        queryset = super(DraftManager, self).get_query_set().filter(status__exact=HawkNews.STATUS_DRAFT)
+        return queryset
+
+
+class PublicManager(models.Manager):
+
+    def get_query_set(self):
+        queryset = super(PublicManager, self).get_query_set().filter(status__exact=HawkNews.STATUS_PUBLIC)
+        return queryset
+
 
 class HawkNews(models.Model):
-    DRAFT = 1
-    PUBLIC = 2
-    DRAFT_CHOICES = (
-        (DRAFT,"Draft",),
-        (PUBLIC,"Public",),
+    STATUS_DRAFT = 1
+    STATUS_PUBLIC = 2
+    STATUS_CHOICES = (
+        (STATUS_DRAFT, _('Draft')),
+        (STATUS_PUBLIC, _('Public')),
     )
     pub_date = models.DateTimeField()
     title = models.CharField(max_length=250)
     slug = models.SlugField(unique=True)
     body = models.TextField()
-    draft = models.IntegerField(choices=DRAFT_CHOICES)
-    
+    status = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=STATUS_PUBLIC)
+
+    objects = models.Manager()
+    published_objects = PublicManager()
+    draft_objects = DraftManager()
+
     class Meta:
-        verbose_name_plural="Hawk News"
-    
+        ordering = ('-pub_date',)
+        verbose_name = _('hawk news')
+        verbose_name_plural = _('hawk news')
+
     def __unicode__(self):
-        return self.slug
-    
+        return self.title
+
     @models.permalink
     def get_absolute_url(self):
-        return ('news_detail', (), { 'year': self.start_datetime.strftime("%Y"),
-                                      'month': self.start_datetime.strftime("%b").lower(),
-                                      'day': self.start_datetime.strftime("%d"),
-                                      'slug': self.slug } )
+        return ('news_detail', (), {
+            'year': self.start_datetime.strftime("%Y"),
+            'month': self.start_datetime.strftime("%b").lower(),
+            'day': self.start_datetime.strftime("%d"),
+            'slug': self.slug})
