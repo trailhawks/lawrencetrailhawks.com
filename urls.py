f3bcff22754f7cd97649e31e8b839ecad7598ce1
jefftriplett <jeff.triplett@gmail.com>
2012-11-15 07:43:28 -0600

    Cleaned up main urls and removed live coverage.

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import include, patterns, url
 from django.contrib import admin
 from django.views.generic.simple import direct_to_template
 
@@ -7,25 +7,24 @@ from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', direct_to_template, {'template': 'homepage.html'}),
-    (r'^live/$', direct_to_template, {'template': 'live_coverage.html'}),
-    (r'^about/$', direct_to_template, {'template': 'about.html'}),
-    (r'^blog/', include('blog.urls')),
-    (r'^comments/', include('django.contrib.comments.urls')),
-    (r'^contact/$', 'members.views.officer_list'),
-    (r'^faq/', include('faq.urls')),
-    (r'^links/', include('links.urls')),
-    (r'^member_list/$', 'members.views.member_list'),
-    (r'^members/', include('members.urls')),
-    (r'^photos/', include('photos.urls')),
-    (r'^races/', include('races.urls')),
-    (r'^runs/', include('runs.urls')),
-    (r'^sponsors/', 'sponsors.views.get_sponsors'),
-    (r'^thanks/$', direct_to_template, {'template': 'thanks.html'}),
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^$', direct_to_template, {'template': 'homepage.html'}),
+    url(r'^about/$', direct_to_template, {'template': 'about.html'}),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^contact/$', 'members.views.officer_list'),
+    url(r'^faq/', include('faq.urls')),
+    url(r'^links/', include('links.urls')),
+    url(r'^member_list/$', 'members.views.member_list'),
+    url(r'^members/', include('members.urls')),
+    url(r'^photos/', include('photos.urls')),
+    url(r'^races/', include('races.urls')),
+    url(r'^runs/', include('runs.urls')),
+    url(r'^sponsors/', 'sponsors.views.get_sponsors'),
+    url(r'^thanks/$', direct_to_template, {'template': 'thanks.html'}),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
+        url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
     )
