fa1bd41ef620cfa6a46f4180a97ec2cd19a0133f
jefftriplett <jeff.triplett@gmail.com>
2012-12-13 21:47:39 -0600

    Added a get_faqs_for_object template tag which looks up FAQ objects for a specific model type and for that specific object.

--- faq/templatetags/faq_tags.py
+++ faq/templatetags/faq_tags.py
@@ -1,4 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 from django.template import Library
 
 from ..models import FAQ
@@ -8,10 +9,16 @@ register = Library()
 
 
 @register.assignment_tag(takes_context=True)
-def get_faqs_by_content_type(context, content_type, ):
+def get_faqs_by_content_type(context, content_type):
     app_label, model = content_type.split('.')
-    content_type = ContentType.objects.get(app_label=app_label, model=model)
-    return FAQ.objects.filter(content_type=content_type)
+    ct = ContentType.objects.get(app_label=app_label, model=model)
+    return FAQ.objects.filter(content_type=ct)
+
+
+@register.assignment_tag(takes_context=True)
+def get_faqs_for_object(context, obj):
+    queryset = FAQ.objects.filter(Q(content_type__app_label=obj._meta.app_label) & Q(Q(object_id__isnull=True) or Q(object_id=obj.pk)))
+    return queryset
 
 
 @register.assignment_tag(takes_context=True)
