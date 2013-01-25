96236cb767cfe53044da56bcce7ac9f51e2cdd6c
Jeff Triplett <jeff.triplett@gmail.com>
2013-01-25 15:13:51 -0600

    Updated sponsor tags.

--- sponsors/templatetags/sponsors_tags.py
+++ sponsors/templatetags/sponsors_tags.py
@@ -1,9 +1,32 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 from django.template import Library
 
 from ..models import Sponsor
 
 
 register = Library()
+
+
+@register.assignment_tag(takes_context=True)
+def get_sponsors_by_content_type(context, content_type):
+    app_label, model = content_type.split('.')
+    ct = ContentType.objects.get(app_label=app_label, model=model)
+    return Sponsor.active_objects.filter(content_type=ct)
+
+
+@register.assignment_tag(takes_context=True)
+def get_sponsors_for_object(context, obj):
+    """Find Sponsors for an object and all model types."""
+    query = Q(content_type__app_label=obj._meta.app_label, object_id=obj.pk)
+    query = query or Q(content_type__app_label=obj._meta.app_label, object_id__isnull=True)
+    queryset = Sponsor.active_objects.filter(query)
+    return queryset
+
+
+@register.assignment_tag(takes_context=True)
+def get_latest_sponsors(context, num):
+    return Sponsor.active_objects.all()[:num]
 
 
 @register.assignment_tag(takes_context=True)
