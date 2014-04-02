ad5387850ed46d414f6208ca7a0f29432d434e38
Jeff Triplett <jeff.triplett@gmail.com>
2014-04-02 00:41:40 -0500

    Updated news alert_status to allow blanks.

--- news/models.py
+++ news/models.py
@@ -30,7 +30,7 @@ class News(models.Model, ShortUrlMixin):
     slug = models.SlugField(blank=True, null=True)
     body = models.TextField()
     status = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=STATUS_PUBLIC)
-    alert_status = models.CharField(max_length=50, choices=ALERT_CHOICES, default='')
+    alert_status = models.CharField(max_length=50, choices=ALERT_CHOICES, default='', blank=True)
 
     # show in main news feed? handy for race results...
     content_type = models.ForeignKey(ContentType, blank=True, null=True)
