758d492c4b10a10cc667c1184ab6242458cb1a9e
Jeff Triplett <jeff.triplett@gmail.com>
2014-04-02 00:42:09 -0500

    Added photo set views and explicit template names.

--- photos/urls.py
+++ photos/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import PhotoDetailView, PhotoListView
+from .views import PhotoDetailView, PhotoListView, PhotoSetDetailView, PhotoSetListView
 
 
 urlpatterns = patterns('',
     url(r'^$', PhotoListView.as_view(), name='photo_list'),
+    url(r'^groups/$', PhotoSetListView.as_view(), name='photoset_list'),
     url(r'^(?P<slug>[-_\w]+)/$', PhotoDetailView.as_view(), name='photo_detail'),
+    url(r'^groups/(?P<flickr_id>[-_\w]+)/$', PhotoSetDetailView.as_view(), name='photoset_detail'),
 )
--- photos/views.py
+++ photos/views.py
@@ -1,14 +1,29 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from syncr.flickr.models import Photo
+from syncr.flickr.models import Photo, PhotoSet
 
 
 class PhotoDetailView(DetailView):
     model = Photo
     navitem = 'photos'
+    template_name = 'photos/photo_detail.html'
 
 
 class PhotoListView(ListView):
     model = Photo
     navitem = 'photos'
     paginate_by = 12
+    template_name = 'photos/photo_list.html'
+
+
+class PhotoSetDetailView(DetailView):
+    model = PhotoSet
+    navitem = 'photosets'
+    template_name = 'photos/photoset_detail.html'
+
+
+class PhotoSetListView(ListView):
+    model = PhotoSet
+    navitem = 'photosets'
+    paginate_by = 12
+    template_name = 'photos/photoset_list.html'
