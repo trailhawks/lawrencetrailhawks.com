a860d495ff746263278e6a4cc88cfc36923c3bbc
Jeff Triplett <jeff.triplett@gmail.com>
2014-04-02 19:04:30 -0500

    Updated event admin to include disable / enable comment actions.

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Event
+from core.actions import disable_comments, enable_comments
 from faq.admin import FaqInline
 from links.admin import LinksInline
 from news.admin import NewsInline
