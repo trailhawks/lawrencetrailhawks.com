353a766ea4684856d951435e08dd2a7abc6c3872
Jeff Triplett <jeff.triplett@gmail.com>
2014-12-27 18:18:03 -0600

    Updated hosts.

--- hosts.py
+++ hosts.py
@@ -6,10 +6,23 @@ host_patterns = patterns(
     '',
     #host(r'lawrencetrailhawks\.dev', settings.ROOT_URLCONF, name='without_www'),
     #host(r'www\.lawrencetrailhawks\.dev', settings.ROOT_URLCONF, name='www'),
-    host(r'hawkhundred', settings.RACE_URLCONF, name='hawkhundred_race'),
-    host(r'hawkhundred\.dev', settings.RACE_URLCONF, name='hawkhundred'),
-    host(r'www\.hawkhundred\.dev', settings.RACE_URLCONF, name='hawkhundred_www'),
 
-    host(r'trailhawks\.dev', settings.ROOT_URLCONF, name='default'),
-    #host(r'www\.trailhawks\.dev', settings.ROOT_URLCONF, name='trailhawks_www'),
+    host(r'(?P<race>[\w\.-]+)', settings.RACE_URLCONF,
+         callback='lawrencetrailhawks.races.callbacks.host_race',
+         name='hawkhundred_race'),
+
+    host(r'hawkhundred\.dev', settings.RACE_URLCONF,
+         callback='lawrencetrailhawks.races.callbacks.host_race',
+         name='hawkhundred'),
+
+    host(r'www\.hawkhundred\.dev', settings.RACE_URLCONF,
+         callback='lawrencetrailhawks.races.callbacks.host_race',
+         name='hawkhundred_www'),
+
+    host(r'lawrencetrailhawks\.dev', settings.ROOT_URLCONF,
+         name='default'),
+
+    host(r'www\.lawrencetrailhawks\.dev', settings.ROOT_URLCONF,
+         name='default_www'),
+
 )
