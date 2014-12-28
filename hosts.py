d37606b90260515d0a643c7bc652d1a294b3559d
Jeff Triplett <jeff.triplett@gmail.com>
2014-12-27 18:39:26 -0600

    Updated hosts

--- hosts.py
+++ hosts.py
@@ -4,25 +4,21 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     '',
-    #host(r'lawrencetrailhawks\.dev', settings.ROOT_URLCONF, name='without_www'),
-    #host(r'www\.lawrencetrailhawks\.dev', settings.ROOT_URLCONF, name='www'),
+    host(r'lawrencetrailhawks\.(com|dev|im)', settings.ROOT_URLCONF,
+         name='default'),
+
+    host(r'(alpha|www)\.lawrencetrailhawks\.(com|dev|im)', settings.ROOT_URLCONF,
+         name='default_www'),
 
     host(r'(?P<race>[\w\.-]+)', settings.RACE_URLCONF,
          callback='lawrencetrailhawks.races.callbacks.host_race',
          name='hawkhundred_race'),
 
-    host(r'hawkhundred\.dev', settings.RACE_URLCONF,
+    host(r'hawkhundred\.(com|dev)', settings.RACE_URLCONF,
          callback='lawrencetrailhawks.races.callbacks.host_race',
          name='hawkhundred'),
 
-    host(r'www\.hawkhundred\.dev', settings.RACE_URLCONF,
+    host(r'www\.hawkhundred\.(com|dev)', settings.RACE_URLCONF,
          callback='lawrencetrailhawks.races.callbacks.host_race',
          name='hawkhundred_www'),
-
-    host(r'lawrencetrailhawks\.dev', settings.ROOT_URLCONF,
-         name='default'),
-
-    host(r'www\.lawrencetrailhawks\.dev', settings.ROOT_URLCONF,
-         name='default_www'),
-
 )
