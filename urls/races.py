205519d878027fc3b7689d9f8c1be2c9ade606ac
Jeff Triplett <jeff.triplett@gmail.com>
2015-01-22 19:46:15 -0600

    Updated race templates

--- urls/races.py
+++ urls/races.py
@@ -17,16 +17,16 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='race_websites/index.html'), name='homepage'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='homepage'),
     url(r'^404/', TemplateView.as_view(template_name='404.html')),
     url(r'^500/', TemplateView.as_view(template_name='500.html')),
-    url(r'^calendar$', TemplateView.as_view(template_name='race_websites/calendar.html'), name='race_calendar'),
-    url(r'^course/$', TemplateView.as_view(template_name='race_websites/course.html'), name='race_course'),
-    url(r'^faqs/$', TemplateView.as_view(template_name='race_websites/faqs.html'), name='race_faqs'),
-    url(r'^gallery/$', TemplateView.as_view(template_name='race_websites/gallery.html'), name='race_gallery'),
-    url(r'^results/$', TemplateView.as_view(template_name='race_websites/results.html'), name='race_results'),
-    url(r'^signup/$', TemplateView.as_view(template_name='race_websites/signup.html'), name='race_signup'),
-    url(r'^travel/$', TemplateView.as_view(template_name='race_websites/travel.html'), name='race_travel'),
+    url(r'^calendar$', TemplateView.as_view(template_name='calendar.html'), name='race_calendar'),
+    url(r'^course/$', TemplateView.as_view(template_name='course.html'), name='race_course'),
+    url(r'^faqs/$', TemplateView.as_view(template_name='faqs.html'), name='race_faqs'),
+    url(r'^gallery/$', TemplateView.as_view(template_name='gallery.html'), name='race_gallery'),
+    url(r'^results/$', TemplateView.as_view(template_name='results.html'), name='race_results'),
+    url(r'^signup/$', TemplateView.as_view(template_name='signup.html'), name='race_signup'),
+    url(r'^travel/$', TemplateView.as_view(template_name='travel.html'), name='race_travel'),
 
     url(r'^blog/', include('blog.urls')),
     url(r'^events/', include('events.urls')),
@@ -40,7 +40,7 @@ urlpatterns = patterns(
     url(r'^sponsors/', include('sponsors.urls')),
 
     url(r'^robots\.txt$', include('robots.urls')),
-    #url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^', include('favicon.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
