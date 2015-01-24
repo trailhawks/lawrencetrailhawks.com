0f943195d376bfbd81258c8daad57dab8be74013
Jeff Triplett <jeff.triplett@gmail.com>
2015-01-24 10:33:04 -0600

    Fixed member_list link in navigation.

--- members/urls.py
+++ members/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns(
     url(r'^$', MemberListView.as_view(), name='member_list'),
     url(r'^preview/$', MemberEmailPreview.as_view(), name='member_email_preview'),
     url(r'^(?P<pk>\d+)/$', MemberDetailView.as_view(), name='member_detail'),
-    url(r'^member_list/$', member_list, name='member_list'),
+    url(r'^member_list/$', member_list, name='admin_member_list'),
 )
