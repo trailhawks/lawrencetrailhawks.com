cdf9ec085adbe7f4f70dfff282ddb32f46609b4a
Jeff Triplett <jeff.triplett@gmail.com>
2015-01-31 16:47:53 -0600

    Added PhotoReviewList.

--- photos/urls.py
+++ photos/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
 
-from .views import PhotoDetailView, PhotoListView, PhotoSetDetailView, PhotoSetListView
+from . import views
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', PhotoListView.as_view(), name='photo_list'),
-    url(r'^groups/$', PhotoSetListView.as_view(), name='photoset_list'),
-    url(r'^(?P<slug>[-_\w]+)/$', PhotoDetailView.as_view(), name='photo_detail'),
-    url(r'^groups/(?P<flickr_id>[-_\w]+)/$', PhotoSetDetailView.as_view(), name='photoset_detail'),
+    url(r'^$', views.PhotoListView.as_view(), name='photo_list'),
+    url(r'^review/$', views.PhotoReview.as_view(), name='photo_review_list'),
+    url(r'^groups/$', views.PhotoSetListView.as_view(), name='photoset_list'),
+    url(r'^(?P<slug>[-_\w]+)/$', views.PhotoDetailView.as_view(), name='photo_detail'),
+    url(r'^groups/(?P<flickr_id>[-_\w]+)/$', views.PhotoSetDetailView.as_view(), name='photoset_detail'),
 )
