f7d3b11700501c189ff7967fa829da6a783477fe
Jeff Triplett <jeff.triplett@gmail.com>
2015-07-18 12:43:48 -0500

    Added our past races to the website.
    Thank you @djangogirls for the inspiration and the license which allowed me to use it:
    
    https://github.com/DjangoGirls/djangogirls/search?utf8=%E2%9C%93&q=past-events&type=Code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
-/* LazyLoad instance */
-$('img').each(function(){
-  $(this).attr('src', $(this).attr('data-src'));
+
+$(document).ready(function() {
+
+    /* LazyLoad instance */
+    $('img').each(function(){
+      $(this).attr('src', $(this).attr('data-src'));
+    });
+
+    $('.show-more a').click(function(){
+        $('.past-races .col-md-12').css('max-height', '100%');
+        $('.past-races .col-md-12').css('overflow', 'visible');
+        $('.show-more').css('display', 'none');
+    });
 });
