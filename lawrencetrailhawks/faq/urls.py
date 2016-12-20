6bd2cbb105015967b21cea6a302e17f4f270cbba
Jeff Triplett <jeff.triplett@gmail.com>
2016-12-19 22:54:44 -0600

    Removes lth namespace

--- lawrencetrailhawks/faq/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .views import FaqDetailView, FaqListView
-
-
-urlpatterns = patterns('',
-    url(r'^$', FaqListView.as_view(), name='faq_list'),
-    url(r'^(?P<pk>\d+)/$', FaqDetailView.as_view(), name='faq_detail'),
-)
