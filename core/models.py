0fd6c397abc7d0d2b425278f444f8d898142c7e3
Jeff Triplett <jeff.triplett@gmail.com>
2016-12-23 02:35:03 -0600

    Updaes module_name to object_name

--- core/models.py
+++ core/models.py
@@ -21,7 +21,7 @@ class MachineTagMixin(models.Model):
 
     def get_machine_tags(self):
         machine_tag_namespace = getattr(settings, 'MACHINE_TAG_NAMESPACE', 'trailhawks')
-        machine_tags = ['{0}:{1}={2}'.format(machine_tag_namespace, self._meta.module_name, self.pk)]
+        machine_tags = ['{0}:{1}={2}'.format(machine_tag_namespace, self._meta.object_name, self.pk)]
         return machine_tags
 
 
